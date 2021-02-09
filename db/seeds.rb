9d269b0960c16a7133381263530e68458f26d74e
github username <rdg.brg@gmail.com>
2021-02-08 23:55:02 -0500

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,8 +28,8 @@ fernanda = User.create(
 )
 
 product_1 = Product.create(
-  title: "Jordan Shoes",
-  price: "75.99",
+  title: "Jordan Shoes - New",
+  price: 75.99,
   image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2020/09/13/5f5ed24abcbb52dc282adab0/m_5f5fdb7ae131646464ec685f.jpg",
   quantity: 1,
   seller: clark
@@ -37,15 +37,15 @@ product_1 = Product.create(
 
 product_2 = Product.create(
   title: "MacBook Pro 2013",
-  price: "650.00",
+  price: 650.00,
   image_url: "https://i.insider.com/596cf74fa47cb51c008b4b2f?width=1074&format=jpeg",
   quantity: 2,
   seller: clark
 )
 
 product_3 = Product.create(
-  title: "Chic Womens Shiny Gold Pointy Toe Tassels Lace Up Low Heel Casual ",
-  price: "39.99",
+  title: "Chic Womens Shiny Gold Pointy Toe Tassels",
+  price: 39.99,
   image_url: "https://i.pinimg.com/474x/02/4c/bf/024cbf910e653ea3a1509733fdcf768a.jpg",
   quantity: 2,
   seller: clark
@@ -53,7 +53,7 @@ product_3 = Product.create(
 
 product_4 = Product.create(
   title: "Monogram Matelassé Leather Zip Pouch",
-  price: "125.00",
+  price: 125.00,
   image_url: "https://image.s5a.com/is/image/saks/0400099508757_PEARLGREY?wid=480&hei=640&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
   quantity: 3,
   seller: jacquelyn
@@ -61,7 +61,7 @@ product_4 = Product.create(
 
 product_5 = Product.create(
   title: "Apple Iphone 7 - 128GB",
-  price: "125.00",
+  price: 125.00,
   image_url: "https://cdn.shopify.com/s/files/1/0019/7387/8850/products/DSC01105_1aa7522e-35c6-4f47-8a8a-c11a1a7621fa_3456x.JPG?v=1542834895",
   quantity: 0,
   seller: jacquelyn
@@ -69,7 +69,7 @@ product_5 = Product.create(
 
 product_6 = Product.create(
   title: "Jordan Shoes - Used",
-  price: "99.99",
+  price: 99.99,
   image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2020/09/13/5f5ed24abcbb52dc282adab0/m_5f5fdb7ae131646464ec685f.jpg",
   quantity: 2,
   seller: jacquelyn
